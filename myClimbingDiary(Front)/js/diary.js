1c0b39b65f42289c1653318b19ed2dbfc4ee5174
Lucia Oseguera <48133614+LuciaOseg@users.noreply.github.com>
2020-03-23 20:42:27 -0600

    Nueva pantalla diario

--- myClimbingDiary(Front)/js/diary.js
+++ myClimbingDiary(Front)/js/diary.js
@@ -1,3 +1,45 @@
+//Mostrar div de diary
+$('#diary').click(function() { 
+  document.getElementById("search_form").style.display = "block";
+});
+
+
+//////
+$('#body-row .collapse').collapse('hide'); 
+
+// Collapse/Expand icon
+$('#collapse-icon').addClass('fa-angle-double-left'); 
+
+// Collapse click
+$('[data-toggle=sidebar-colapse]').click(function() {
+    SidebarCollapse();
+});
+
+function SidebarCollapse () {
+    $('.menu-collapsed').toggleClass('d-none');
+    $('.sidebar-submenu').toggleClass('d-none');
+    $('.submenu-icon').toggleClass('d-none');
+    $('#sidebar-container').toggleClass('sidebar-expanded sidebar-collapsed');
+    
+    // Treating d-flex/d-none on separators with title
+    var SeparatorTitle = $('.sidebar-separator-title');
+    if ( SeparatorTitle.hasClass('d-flex') ) {
+        SeparatorTitle.removeClass('d-flex');
+    } else {
+        SeparatorTitle.addClass('d-flex');
+    }
+    
+    // Collapse/Expand icon
+    $('#collapse-icon').toggleClass('fa-angle-double-left fa-angle-double-right');
+}
+
+
+
+
+
+/////////////////
+
+
 //Jala la fecha
 Date.prototype.toDateInputValue = (function() {
     var local = new Date(this);
--- myClimbingDiary(Front)/js/index.js
+++ myClimbingDiary(Front)/js/index.js
@@ -23,7 +23,7 @@ $('#login_button').on('click', function(){
     success: function(data){
       // guardar token en localstorage o cookie
       localStorage.setItem('token', data.token)
-      window.location = 'climbingDiary.html'
+      window.location = 'climbingDiary2.html'
     },
     error: function(error_msg) {
       //alert((error_msg['responseText']));
