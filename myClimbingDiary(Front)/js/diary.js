31f3c7a201a18aa0bc30b8e4d7cdb81ab0fcf272
Daniela Garcia <mdgarciaaguilar@hotmail.com>
2020-03-25 13:15:04 -0600

    design changes

--- myClimbingDiary(Front)/js/diary.js
+++ myClimbingDiary(Front)/js/diary.js
@@ -233,7 +233,7 @@ $('#btn_save').on('click', function(){
           success: function(data){
             alert("Registro agregado con exito");
             console.log('success: '+ data);
-            window.location = 'climbingDiary.html'
+            window.location = 'climbingDiary2.html'
           },
           error: function(error_msg) {
             console.log(json_to_send);
@@ -280,7 +280,7 @@ $(document).on('click', '#btn_delete', function(event) {
     success: function(){
       alert("Registro Eliminado");
       console.log('Deleted ');
-      window.location = 'climbingDiary.html'
+      window.location = 'climbingDiary2.html'
     },
     error: function(error_msg) {
         alert((error_msg['responseText']));
@@ -338,7 +338,7 @@ $(document).on('click','#btn_patch', function(event){
           success: function(data){
             alert("Registro editado con exito");
             console.log('Edit: '+ data + "id: "+ id);
-            window.location = 'climbingDiary.html'
+            window.location = 'climbingDiary2.html'
           },
           error: function(error_msg) {
             console.log(json_to_send);
