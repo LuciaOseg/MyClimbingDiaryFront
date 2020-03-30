e0856b2d7767124521db86d90a5c30ba9f28291d
Daniela Garcia <mdgarciaaguilar@hotmail.com>
2020-03-29 19:35:20 -0600

    my next project, se puede filtrar solo por grado o solo por ruta o ambos

--- myClimbingDiary(Front)/js/nextproject.js
+++ myClimbingDiary(Front)/js/nextproject.js
@@ -9,15 +9,18 @@ $.ajax({
 
   success: function(data){
     rutas = data;
-
+    let zonas = data;
+    let zonasUnicas = Array.from(new Set(zonas.map(a => a.zona)));
+    let gradosUnicos = Array.from(new Set(zonas.map(a => a.grado)));
+    
     // Llenar select de zonas 
     let nuevo_html = " ";
     nuevo_html += `<option id="nulo"></option>`;
-    for (var i = 0; i < data.length; i++) {
+    for (var i = 0; i < zonasUnicas.length; i++) {
       fields = data
       nuevo_html += `
-      <option value="${data[i].zona}">
-        ${data[i].zona}
+      <option value="${zonasUnicas[i]}">
+        ${zonasUnicas[i]}
       </option>
       `
     }
@@ -26,18 +29,15 @@ $.ajax({
     // Llenar select de grados
     nuevo_html = " ";
     nuevo_html += `<option id="nulo"></option>`;
-    for (var i = 0; i < data.length; i++) {
+    for (var i = 0; i < gradosUnicos.length; i++) {
       fields = data
       nuevo_html += `
-      <option value="${data[i].grado}">
-        ${data[i].grado}
+      <option value="${gradosUnicos[i]}">
+        ${gradosUnicos[i]}
       </option>
       `
     }
     $("#selectGrado").append(nuevo_html);
-    // console.log(rutas);
-    // console.log('ohoh')
-    // console.log(data);
   },
   error: function(error_msg){
     console.error(error_msg)
@@ -45,8 +45,6 @@ $.ajax({
 });
 }
 getFields();
-// console.log('afuera')
-// console.log(rutas);
 //Jala el token para futuras acciones
 var token = localStorage.getItem('token');
 if (token) {
@@ -55,43 +53,98 @@ if (token) {
 
 // Search
 $('#btn-search').on('click', function(){
-  console.log("search");
-  //console.log(rutas)
+  $("#resultados_busqueda").empty();
 
   let zona = $('#selectZona').children('option:selected').val();
   let grado = $('#selectGrado').children('option:selected').val();
 
-  console.log(zona, grado);
+  
 
   $("#resultados_busqueda").innerHTML = "";
   let nuevo_html = " ";
   for (var i = 0; i < rutas.length; i++) {
-    //console.log(rutas[i].zona);
-    if(rutas[i].zona == zona && rutas[i].grado == grado) {
-      console.log('hola')
-      nuevo_html += `
-      <div class="col-sm-3">
-        <div class="card bg-light">
-          <div class="card-header">
-            <h5>${rutas[i].zona}</h5>
-          </div>
-            <div class="card-body">
-              <p class="card-text">
-                <strong>Ruta:</strong> ${rutas[i].nombre}
-              </p>
-              <p class="card-text">
-                <strong>Grado:</strong> ${rutas[i].grado}
-              </p>
-              <button type="button" class="btn btn-primary">
-                Add
-              </button>
+    // Si hay zona y grado
+    if (zona != '' && grado != '') {
+      if(rutas[i].zona == zona && rutas[i].grado == grado) {
+        nuevo_html += `
+        <div class="col-sm-3">
+          <div class="card bg-light">
+            <div class="card-header">
+              <h5>${rutas[i].zona}</h5>
             </div>
+              <div class="card-body">
+                <p class="card-text">
+                  <strong>Ruta:</strong> ${rutas[i].nombre}
+                </p>
+                <p class="card-text">
+                  <strong>Grado:</strong> ${rutas[i].grado}
+                </p>
+                <button type="button" class="btn btn-primary">
+                  Add
+                </button>
+              </div>
+          </div>
+          <br>
         </div>
-        <br>
-      </div>
-      `
+        `
+      }
+    } 
+    // si solo hay zona
+    if (zona != '' && grado == '') {
+      if (rutas[i].zona == zona) {
+        nuevo_html += `
+        <div class="col-sm-3">
+          <div class="card bg-light">
+            <div class="card-header">
+              <h5>${rutas[i].zona}</h5>
+            </div>
+              <div class="card-body">
+                <p class="card-text">
+                  <strong>Ruta:</strong> ${rutas[i].nombre}
+                </p>
+                <p class="card-text">
+                  <strong>Grado:</strong> ${rutas[i].grado}
+                </p>
+                <button type="button" class="btn btn-primary">
+                  Add
+                </button>
+              </div>
+          </div>
+          <br>
+        </div>
+        `
+      }
     }
-  }
+    // si solo hay grado
+    if (grado != '' && zona == '') {
+      if (rutas[i].grado == grado) {
+        nuevo_html += `
+        <div class="col-sm-3">
+          <div class="card bg-light">
+            <div class="card-header">
+              <h5>${rutas[i].zona}</h5>
+            </div>
+              <div class="card-body">
+                <p class="card-text">
+                  <strong>Ruta:</strong> ${rutas[i].nombre}
+                </p>
+                <p class="card-text">
+                  <strong>Grado:</strong> ${rutas[i].grado}
+                </p>
+                <button type="button" class="btn btn-primary">
+                  Add
+                </button>
+              </div>
+          </div>
+          <br>
+        </div>
+        `
+      }
+    }
+    
+
+    }
+    
   if(nuevo_html == ""){
     alert("No se encontraron rutas");
   } else {
