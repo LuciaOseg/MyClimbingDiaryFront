9155317bbce8c68e425fe6b04ca8184f6d5a42e9
lourdesnrdz <lulynavarrete@hotmail.com>
2020-05-20 12:22:09 -0500

    chart1

--- myClimbingDiary(Front)/js/myprogress.js
+++ myClimbingDiary(Front)/js/myprogress.js
@@ -1,4 +1,10 @@
 
+//Jala el token para futuras acciones
+var token = localStorage.getItem('token');
+if (token) {
+  token = token.replace(/^"(.*)"$/, '$1'); // Remove quotes from token start/end.
+}
+
 //Logout
 $('#btn_logout').on('click', function(){
     console.log("Logout");
@@ -22,3 +28,56 @@ $('#btn_logout').on('click', function(){
   });
 
 })
+
+
+var entries = [];
+
+var rutas = [];
+var pegues = [];
+
+function getEntries(){
+  //$("#nombre_rutas").children("option").remove();
+  $.ajax({
+    url: "https://myclimbingdiary.herokuapp.com/proyectos",
+    headers: {
+       'Content-Type':'application/json',
+       'Authorization': 'Bearer ' + token
+   },
+    method: "GET",
+    dataType: "json",
+
+
+    success: function(data){
+      entries = data;
+      console.log(data)
+
+      // datos para las chart 1
+      // nombres de las rutas y número de pegues
+      for(i = 0; i < entries.length; i++) {
+        rutas.push(entries[i].nombre);
+        pegues.push(parseInt(entries[i].pegues[0].numeroPegues));
+      }
+     
+      dataChart1()
+    },
+    error: function(error_msg){
+      console.error(error_msg)
+    }
+  });
+}
+getEntries();
+
+var data 
+
+function dataChart1() {
+  data = {
+    labels: rutas,
+    series: [pegues]
+  };
+  console.log(data)
+
+  new Chartist.Bar('#chart1', data);
+}
+
+
+
